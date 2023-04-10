a5af0ce2a3691136d1a40698a0223256e1f386a2
Daniela De Simone <dsdesimone8@gmail.com>
2023-04-10 16:27:14 -0300

    Loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,10 +4,28 @@ const Loading = () => {
 
   
   return (
-    <div className="loader__container">
-        <div className="dot"></div>
-        <div className='loading'>LOADING</div>
-    </div>
+    <>
+      <div className="loader__container">
+          <div className="dot"></div>
+          <div className='loading'>LOADING</div>
+      </div>
+      <div class="fondo-animado">
+        <span class="uno delay-1"><i class="fa-solid fa-cloud"></i></span>
+        <span class="dos"><i class="fa-solid fa-bolt"></i></span>
+        <span class="tres delay-5"><i class="fa-solid fa-sun"></i></span>
+        <span class="cuatro delay-1"><i class="fa-regular fa-snowflake"></i></span>
+
+        <span class="uno delay-3"><i class="fa-regular fa-snowflake"></i></span>
+        <span class="dos delay-7"><i class="fa-solid fa-cloud"></i></span>
+        <span class="tres delay-5"><i class="fa-solid fa-bolt"></i></span>
+        <span class="cuatro delay-4"><i class="fa-solid fa-sun"></i></span>
+
+        <span class="uno delay-2"><i class="fa-solid fa-sun"></i></span>
+        <span class="dos delay-3"><i class="fa-regular fa-snowflake"></i></span>
+        <span class="tres delay-6"><i class="fa-solid fa-cloud"></i></span>
+        <span class="cuatro delay-3"><i class="fa-solid fa-bolt"></i></span>  
+      </div>
+    </>
   )
 }
 
